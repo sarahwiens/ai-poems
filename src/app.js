9ab9446fc7757b2c79044e187e9da0699c21c555
sarahwiens <sarahwiens88@gmail.com>
2024-12-18 21:42:31 -0500

    added SheCodes AI by-line to the poem

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let topicInput = document.querySelector("#poem-topic-input");
   let apiKey = "7f314bd46t448eb65o54002ab9dadc03";
   let context =
-    "You are a highly skilled and creative poet. You have an excellent sense of humor. You can write a limerick from any given topic. Your response consists of five lines in a single stanza with a rhyme scheme of AABBA. Please separate each line with <br />";
+    "You are a highly skilled and creative poet. You have an excellent sense of humor. You can write a limerick from any given topic. Your response consists of five lines in a single stanza with a rhyme scheme of AABBA. Please separate each line with <br />. At the end of your poem, please add '<br/><strong>SheCodes AI</strong>'.";
   let prompt = `Please provide a limerick about ${topicInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiURL).then(displayLimerick);
